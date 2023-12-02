b9ab7300d76309c6ea77e24ef5775317a5f24802
pabloivorra <145840713+pabloivorra@users.noreply.github.com>
2023-12-02 11:45:02 +0100

    Update conexionmongo.py

--- conexionmongo.py
+++ conexionmongo.py
@@ -3,20 +3,15 @@ import pymongo
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Si deseas utilizar seaborn para las gráficas
-# import seaborn as sns
-
 # Establecer la conexión con MongoDB
 client = pymongo.MongoClient('mongodb://root:example@localhost:27017/')
 
 # Elegir la base de datos y la colección que deseas utilizar
-# (Reemplaza 'tu_base_de_datos' y 'tu_colección' con los nombres reales)
-db = client['myDatabase']
-collection = db['myCollection']
+db = client['myDatabase']  # Asegúrate de que 'myDatabase' es el nombre real de tu base de datos
+collection = db['myCollection']  # Asegúrate de que 'myCollection' es el nombre real de tu colección
 
-# Realizar una consulta para obtener todos los datos de la colección
-# (o ajustar la consulta según tus necesidades)
-data = collection.find()
+# Realizar una consulta para obtener los campos 'address' y 'available' de todos los documentos
+data = collection.find({}, {'address': 1, 'available': 1, '_id': 0})
 
 # Convertir los datos a un DataFrame de Pandas
 df = pd.DataFrame(list(data))
@@ -24,14 +19,18 @@ df = pd.DataFrame(list(data))
 # Cerrar la conexión de cliente MongoDB
 client.close()
 
-# Asegúrate de que la columna que vas a graficar está en el formato correcto
-# Por ejemplo, si es una fecha o un número, conviértelo al tipo de datos apropiado
+# Asegúrate de que la columna 'available' esté en el formato correcto (numérico)
+df['available'] = pd.to_numeric(df['available'], errors='coerce')
+df.dropna(subset=['available'], inplace=True)  # Eliminar filas con valores no numéricos o faltantes
 
-# Realizar una gráfica (cambia 'campo_x' y 'campo_y' por los nombres de tus campos)
-plt.figure(figsize=(10, 6))
-plt.plot(df['campo_x'], df['campo_y'], marker='o')
-plt.title('Título de la Gráfica')
-plt.xlabel('Campo X')
-plt.ylabel('Campo Y')
-plt.grid(True)
+# Ordenar el DataFrame por la cantidad de bicicletas disponibles para mejor visualización
+df.sort_values(by='available', ascending=True, inplace=True)
+
+# Crear un gráfico de barras
+plt.figure(figsize=(10, 8))
+plt.barh(df['address'], df['available'])  # Gráfico de barras horizontal
+plt.xlabel('Cantidad de Bicicletas Disponibles')
+plt.title('Disponibilidad de Bicicletas por Dirección')
+plt.gca().invert_yaxis()  # Invertir el eje Y para mostrar la barra más alta en la parte superior
+plt.tight_layout()  # Ajustar la disposición para que todo encaje bien en el gráfico
 plt.show()
